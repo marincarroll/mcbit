0c78e2cb33c5f54835e1cf5201cdcdbd465691eb
Marin @Home <marin@marincarroll.com>
2022-06-02 10:00:28 -0400

    Feat: add language file and add and make-pot to build

--- functions.php
+++ functions.php
@@ -10,6 +10,14 @@ define( 'MCBIT_ASSETS_DIR', get_template_directory() . '/assets/' );
 define( 'MCBIT_INC_DIR', get_template_directory() . '/inc/' );
 
 /**
+ * Load i18n file
+ */
+function mcbit_load_pot() {
+    load_theme_textdomain( 'mcbit', get_template_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'mcbit_load_pot' );
+
+/**
  * Includes
  */
 require MCBIT_INC_DIR . 'enqueue.php';
