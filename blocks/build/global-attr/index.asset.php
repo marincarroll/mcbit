94a6f3a392926e43124a9c54766f336785630a78
Marin @Home <marin@marincarroll.com>
2022-06-02 10:01:03 -0400

    Build

--- blocks/build/global-attr/index.asset.php
+++ blocks/build/global-attr/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blockEditor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f0fcbab68926c9919ab7');
+<?php return array('dependencies' => array('wp-blockEditor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '61c0119a928dc932af35');
